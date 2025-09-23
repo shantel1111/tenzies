3c1da5f58afaea382c8c0144e93e54f309bd9d31
shantel1111 <148761452+shantel1111@users.noreply.github.com>
2025-09-23 13:57:10 -0300

    Added new features
    - A timer and roll counter
    - CSS styling for features container
    - Fixed tab indentation

--- App.jsx
+++ App.jsx
@@ -1,74 +1,118 @@
-import { useState, useRef, useEffect } from "react"
-import Die from "./Die"
-import { nanoid } from "nanoid"
-import Confetti from "react-confetti"
+import { useState, useRef, useEffect } from "react";
+import Die from "./Die";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
-    const [dice, setDice] = useState(() => generateAllNewDice())
-    const buttonRef = useRef(null)
+  const [dice, setDice] = useState(() => generateAllNewDice());
+  const buttonRef = useRef(null);
 
-    const gameWon = dice.every(die => die.isHeld) &&
-        dice.every(die => die.value === dice[0].value)
-        
-    useEffect(() => {
-        if (gameWon) {
-            buttonRef.current.focus()
-        }
-    }, [gameWon])
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
 
-    function generateAllNewDice() {
-        return new Array(10)
-            .fill(0)
-            .map(() => ({
-                value: Math.ceil(Math.random() * 6),
-                isHeld: false,
-                id: nanoid()
-            }))
+  useEffect(() => {
+    if (gameWon) {
+      buttonRef.current.focus();
     }
-    
-    function rollDice() {
-        if (!gameWon) {
-            setDice(oldDice => oldDice.map(die =>
-                die.isHeld ?
-                    die :
-                    { ...die, value: Math.ceil(Math.random() * 6) }
-            ))
-        } else {
-            setDice(generateAllNewDice())
-        }
+  }, [gameWon]);
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    }));
+  }
+
+  const timerRef = useRef(null);
+  const [gameActive, setGameActive] = useState(false);
+  const [rolls, setRolls] = useState(0);
+  const [time, setTime] = useState(0);
+
+  useEffect(
+    function () {
+      if (gameActive) {
+        timerRef.current = setInterval(
+          () => setTime((oldTime) => oldTime + 1),
+          1000
+        );
+      }
+
+      return () => {
+        clearInterval(timerRef.current);
+      };
+    },
+    [gameActive]
+  );
+
+  useEffect(
+    function () {
+      if (gameWon) {
+        setGameActive(false);
+      }
+    },
+    [gameWon]
+  );
+
+  function rollDice() {
+    if (rolls === 0) {
+      setGameActive(true);
     }
 
-    function hold(id) {
-        setDice(oldDice => oldDice.map(die =>
-            die.id === id ?
-                { ...die, isHeld: !die.isHeld } :
-                die
-        ))
+    if (!gameWon) {
+      setRolls((oldRoll) => oldRoll + 1);
+      setDice((oldDice) =>
+        oldDice.map((die) =>
+          die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+        )
+      );
+    } else {
+      setRolls(0);
+      setTime(0);
+      setGameActive(false);
+      setDice(generateAllNewDice());
     }
+  }
 
-    const diceElements = dice.map(dieObj => (
-        <Die
-            key={dieObj.id}
-            value={dieObj.value}
-            isHeld={dieObj.isHeld}
-            hold={() => hold(dieObj.id)}
-        />
-    ))
+  function hold(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
 
-    return (
-        <main>
-            {gameWon && <Confetti />}
-            <div aria-live="polite" className="sr-only">
-                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
-            </div>
-            <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-            <div className="dice-container">
-                {diceElements}
-            </div>
-            <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
-                {gameWon ? "New Game" : "Roll"}
-            </button>
-        </main>
-    )
+  const diceElements = dice.map((dieObj) => (
+    <Die
+      key={dieObj.id}
+      value={dieObj.value}
+      isHeld={dieObj.isHeld}
+      hold={() => hold(dieObj.id)}
+    />
+  ));
+
+  return (
+    <main>
+      {gameWon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && (
+          <p>Congratulations! You won! Press "New Game" to start again.</p>
+        )}
+      </div>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="progress-container">
+        <span ref={timerRef}>Timer: 0.{time}s</span>
+        <span>Roll Counter: {rolls}</span>
+      </div>
+      <div className="dice-container">{diceElements}</div>
+      <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
+        {gameWon ? "New Game" : "Roll"}
+      </button>
+    </main>
+  );
 }
--- Die.jsx
+++ Die.jsx
@@ -1,15 +1,17 @@
 export default function Die(props) {
-    const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
-    }
-    
-    return (
-        <button 
-            style={styles}
-            onClick={props.hold}
-            aria-pressed={props.isHeld}
-            aria-label={`Die with value ${props.value}, 
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+
+  return (
+    <button
+      style={styles}
+      onClick={props.hold}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
             ${props.isHeld ? "held" : "not held"}`}
-        >{props.value}</button>
-    )
+    >
+      {props.value}
+    </button>
+  );
 }
